8743a99d2b4c624e4f5e3775ce88524b85a8bca9
Victor H. Salcedo <victorsalc2@gmail.com>
2021-09-16 01:04:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const suits = ["♦", "♥", "♠", "♣"];
+//const cardSuit = document.querySelectorAll(".card-suit");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  render();
+};
+
+let render = () => {
+  let randomCard = Math.floor(Math.random() * cards.length);
+  document.getElementById("card-number").innerHTML = cards[randomCard];
+
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  let cardSuit = suits[randomSuit];
+  if (cardSuit == "♥" || cardSuit == "♦") {
+    document.querySelector(".card").style.color = "red";
+  }
+  document.getElementById("top-suit").innerHTML = cardSuit;
+  document.getElementById("bottom-suit").innerHTML = cardSuit;
 };
